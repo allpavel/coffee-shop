4dcc3daa04bec673bf310d3121c11c2010adc802
Pavel Alloyarov <allpavel@yandex.ru>
2023-02-15 22:30:07 +0300

    add footer bottom

---
+++ components/FooterBottom/FooterBottom.tsx
@@ -0,0 +1,19 @@
+import styles from './FooterBottom.module.css';
+import { FaInstagram, FaTwitter, FaFacebookF, FaTumblr, FaVimeoV, FaLinkedinIn } from 'react-icons/fa';
+
+export const FooterBottom = () => {
+  return (
+    <section className={styles.credentials}>
+      <div>© Pavel Alloyrov</div>
+      <div className={styles.socials}>
+        <FaInstagram className={styles.socials__icon} />
+        <FaTwitter className={styles.socials__icon} />
+        <FaFacebookF className={styles.socials__icon} />
+        <FaTumblr className={styles.socials__icon} />
+        <FaVimeoV className={styles.socials__icon} />
+        <FaLinkedinIn className={styles.socials__icon} />
+      </div>
+      <div>2023 All Rights Reserved</div>
+    </section>
+  );
+};
