99c142ddf483a68e1514c41bf413082953f81b51
Pavel Alloyarov <allpavel@yandex.ru>
2023-02-21 20:53:57 +0300

    add the brewing methods section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { Hero } from '@/components/Hero/Hero';
 import { BookingSection } from '@/components/BookingSection/BookingSection';
 import { AboutUs } from '@/components/AboutUs/AboutUs';
 import { AboutApp } from '@/components/AboutApp/AboutApp';
+import { BrewingMethods } from '@/components/BrewingMethods/BrewingMethods';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <BookingSection />
       <AboutUs />
       <AboutApp />
+      <BrewingMethods />
     </>
   );
 }
