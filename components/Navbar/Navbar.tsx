f87317a6dae9ca22ae6921018971df8b4fff9016
Pavel Alloyarov <allpavel@yandex.ru>
2023-02-27 22:28:43 +0300

    add changes of navbar when scroll is over 400

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,11 +1,27 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { throttle } from '@/utills/throttle';
 import logo from '../../public/images/logo-2.png';
 import styles from './Navbar.module.css';
 
 export const Navbar = () => {
+  const [position, setPosition] = useState(0);
+
+  const handleNavBarChange = () => {
+    setPosition(window.scrollY);
+  };
+
+  const throttledhHandleNavBarChange = throttle(handleNavBarChange, 200);
+
+  useEffect(() => {
+    window.addEventListener('scroll', throttledhHandleNavBarChange, { passive: true });
+
+    return () => removeEventListener('scroll', throttledhHandleNavBarChange);
+  }, []);
+
   return (
-    <header className={styles.container}>
+    <header className={`${styles.container} ${position > 400 ? styles.shadow : ''}`}>
       <Link href="#">
         <Image src={logo} alt={'Logo of the coffee shop'} />
       </Link>
