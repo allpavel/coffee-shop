71a99299ebda4febd8bf1cc0d9b46961f21a69bb
Pavel Alloyarov <allpavel@yandex.ru>
2023-03-02 22:08:00 +0300

    add the benefits section to the about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,4 +1,5 @@
 import { AboutUs } from '@/components/AboutUs/AboutUs';
+import { Benefits } from '@/components/Pages/AboutPage/Benefits/Benefits';
 import { Hero } from '@/components/Pages/AboutPage/Hero/Hero';
 import { Hero2 } from '@/components/Pages/AboutPage/Hero2/Hero2';
 
@@ -8,6 +9,7 @@ export default function About() {
       <Hero />
       <AboutUs />
       <Hero2 />
+      <Benefits />
     </>
   );
 }
