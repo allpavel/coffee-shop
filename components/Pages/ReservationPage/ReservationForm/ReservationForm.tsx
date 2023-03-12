98af3821ec91a9407cf61844c01411ce6a4c6b25
Pavel Alloyarov <allpavel@yandex.ru>
2023-03-12 22:32:56 +0300

    add zod validation

--- components/Pages/ReservationPage/ReservationForm/ReservationForm.tsx
+++ components/Pages/ReservationPage/ReservationForm/ReservationForm.tsx
@@ -1,25 +1,30 @@
 import { useForm, SubmitHandler } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
 import styles from './ReservationForm.module.css';
 
-type Inputs = {
-  name: string;
-  phone: string;
-  seats: string;
-  date: string;
-  time: string;
-  email: string;
-  notes: string;
-};
+const Input = z.object({
+  name: z.string({ required_error: 'Name is required' }).trim(),
+  phone: z.string({ required_error: 'Phone is required' }).trim(),
+  seats: z.coerce.number().gt(0, { message: 'Must be greater than 0' }).lt(10, { message: 'Must be less than 10' }),
+  date: z.string({ required_error: 'Date is required' }).trim(),
+  time: z.string({ required_error: 'Time is required' }).trim(),
+  email: z.string({ required_error: 'Email is required' }).email().trim(),
+  notes: z.string().trim(),
+});
+
+type Inputs = z.infer<typeof Input>;
 
 export const ReservationForm = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<Inputs>();
+  } = useForm<Inputs>({ resolver: zodResolver(Input) });
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {
-    console.log(data);
+    console.log(Input.parse(data));
   };
 
   return (
@@ -34,16 +39,27 @@ export const ReservationForm = () => {
             seas. Together likeness after he unto, day lesser unto good them male make all. Sea so evening To don't our.
           </p>
         </div>
-        <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className={styles.form_content}>
-            <input type="text" placeholder="Name*" className={styles.form_input} />
-            <input type="text" placeholder="Phone*" className={styles.form_input} />
-            <input type="text" placeholder="Seats*" className={styles.form_input} />
-            <input type="date" placeholder="dd.mm.yyyy" className={styles.form_input} />
-            <input type="text" placeholder="Time" className={styles.form_input} />
-            <input type="text" placeholder="Email*" className={styles.form_input} />
+            <input type="text" placeholder="Name*" className={styles.form_input} {...register('name')} />
+            {errors.name?.message && <p>{errors.name.message}</p>}
+            <input type="text" placeholder="Phone*" className={styles.form_input} {...register('phone')} />
+            {errors.phone?.message && <p>{errors.phone.message}</p>}
+            <input type="number" placeholder="Seats*" className={styles.form_input} {...register('seats')} />
+            {errors.seats?.message && <p>{errors.seats.message}</p>}
+            <input type="date" placeholder="dd.mm.yyyy" className={styles.form_input} {...register('date')} />
+            {errors.date?.message && <p>{errors.date.message}</p>}
+            <input type="text" placeholder="Time" className={styles.form_input} {...register('time')} />
+            {errors.time?.message && <p>{errors.time.message}</p>}
+            <input type="text" placeholder="Email*" className={styles.form_input} {...register('email')} />
+            {errors.email?.message && <p>{errors.email.message}</p>}
           </div>
-          <textarea placeholder="Notes" rows="5" className={`${styles.form_input} ${styles.form_textarea}`} />
+          <textarea
+            placeholder="Notes"
+            rows={5}
+            className={`${styles.form_input} ${styles.form_textarea}`}
+            {...register('notes')}
+          />
           <input type="submit" className={`${styles.form_input} ${styles.form_button}`} />
         </form>
       </div>
