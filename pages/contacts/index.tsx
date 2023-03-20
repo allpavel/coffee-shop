e9cd76b9dbb2a5eed24f6cbd5fbae9ab4596d7e7
Pavel Alloyarov <allpavel@yandex.ru>
2023-03-21 00:48:13 +0300

    add the contact form

--- pages/contacts/index.tsx
+++ pages/contacts/index.tsx
@@ -1,9 +1,11 @@
+import { ContactsContainer } from '@/components/Pages/ContactsPage/ContactsContainer/ContactsContainer';
 import { Hero } from '@/components/Pages/ContactsPage/Hero/Hero';
 
 export default function Contact() {
   return (
     <>
       <Hero />
+      <ContactsContainer />
     </>
   );
 }
