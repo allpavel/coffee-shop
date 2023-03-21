5c5940638225b4f6989b2979e913a7500f92b144
Pavel Alloyarov <allpavel@yandex.ru>
2023-03-21 22:21:23 +0300

    add scroll to top

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,11 +1,13 @@
 import { BlogSection } from '@/components/Pages/BlogPage/BlogSection/BlogSection';
 import { Hero } from '@/components/Pages/BlogPage/Hero/Hero';
+import { ScrollToTop } from '@/components/ScrollToTop/ScrollToTop';
 
 export default function BlogPage() {
   return (
     <>
       <Hero />
       <BlogSection />
+      <ScrollToTop />
     </>
   );
 }
