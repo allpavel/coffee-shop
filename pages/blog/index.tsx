31b2c679b149e917a7a71fdacb40147487421b71
Pavel Alloyarov <allpavel@yandex.ru>
2023-03-23 22:54:40 +0300

    add static props to the blog

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,13 +1,36 @@
-import { BlogSection } from '@/components/Pages/BlogPage/BlogSection/BlogSection';
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
 import { Hero } from '@/components/Pages/BlogPage/Hero/Hero';
-import { ScrollToTop } from '@/components/ScrollToTop/ScrollToTop';
+// import { BlogSection } from '@/components/Pages/BlogPage/BlogSection/BlogSection';
+// import { ScrollToTop } from '@/components/ScrollToTop/ScrollToTop';
 
-export default function BlogPage() {
+export default function BlogPage({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
       <Hero />
-      <BlogSection />
-      <ScrollToTop />
+      {/* <BlogSection />
+      <ScrollToTop /> */}
     </>
   );
 }
+
+export const getStaticProps = async () => {
+  const basicPath = path.join(process.cwd(), 'pages/blog/posts');
+  const files = fs.readdirSync(basicPath);
+  const posts = files.map((file) => {
+    const slug = file.replace('.md', '');
+    const markdownMeta = fs.readFileSync(`${basicPath}/${file}`, 'utf-8');
+    const { data: frontmatter } = matter(markdownMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: { posts },
+  };
+};
