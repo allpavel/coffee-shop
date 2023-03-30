8ad7b40109a11c09405352fe2eeef44c4c6186f9
Pavel Alloyarov <allpavel@yandex.ru>
2023-03-30 21:19:36 +0300

    add new font to all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Layout } from '@/components/Layout/Layout';
+import { open_sans } from '@/fonts/font';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <main className={open_sans.className}>
+        <Component {...pageProps} />
+      </main>
     </Layout>
   );
 }
